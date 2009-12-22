5f438ec6b5dba76e925cf012f5c6830924610553
troll.freedcpp <troll.freedcpp@98094be4-8a28-11de-a133-9d545b4d7674>
2009-12-22 10:01:54 +0000

    Ревизия 338 (linuxdcpp, add path separator between HOME and Downloads for default download dir setting).

--- trunk/linux/hub.cc
+++ trunk/linux/hub.cc
@@ -1411,7 +1411,7 @@ void Hub::onSendMessage_gui(GtkEntry *entry, gpointer data)
 		}
 		else if (command == _("freedcpp"))
 		{
-			hub->addStatusMessage_gui(_("freedcpp 0.0.1.45/0.75, project home: http://freedcpp.narod.ru http://code.google.com/p/freedcpp"), Msg::SYSTEM, Sound::NONE);
+			hub->addStatusMessage_gui(_("freedcpp 0.0.1.46/0.75, project home: http://freedcpp.narod.ru http://code.google.com/p/freedcpp"), Msg::SYSTEM, Sound::NONE);
 		}
 		else if (command == _("help"))
 		{
