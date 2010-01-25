0b889b51e2f9657e9d8655cb7732498f319190db
troll.freedcpp <troll.freedcpp@98094be4-8a28-11de-a133-9d545b4d7674>
2010-01-25 06:26:42 +0000

    Установка типа операции для диалогов: выбор файла, выбор каталога.

--- linux/hub.cc
+++ linux/hub.cc
@@ -1433,7 +1433,7 @@ void Hub::onSendMessage_gui(GtkEntry *entry, gpointer data)
 		}
 		else if (command == _("freedcpp"))
 		{
-			hub->addStatusMessage_gui(_("freedcpp 0.0.1.68/0.75, project home: http://freedcpp.narod.ru http://code.google.com/p/freedcpp"), Msg::SYSTEM, Sound::NONE);
+			hub->addStatusMessage_gui(_("freedcpp 0.0.1.69/0.75, project home: http://freedcpp.narod.ru http://code.google.com/p/freedcpp"), Msg::SYSTEM, Sound::NONE);
 		}
 		else if (command == _("help"))
 		{
