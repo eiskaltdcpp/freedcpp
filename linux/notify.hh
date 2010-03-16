4c0e7523b105ecbf5ee2678422a7b1e90bc02fc1
troll.freedcpp <troll.freedcpp@98094be4-8a28-11de-a133-9d545b4d7674>
2010-03-16 10:25:45 +0000

    Обновил дату.

--- linux/notify.hh
+++ linux/notify.hh
@@ -27,7 +27,7 @@
  * [08.11.09] исправлена ошибка, после выхода не закрывалось уведомление.
  * [08.11.09] исправлена ошибка, не обновлялась иконка.
  *
- * copyright author patch: troll, freedcpp, http://code.google.com/p/freedcpp
+ * Copyright © 2009-2010, author patch: troll, freedcpp, http://code.google.com/p/freedcpp
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- linux/transfers.cc
+++ linux/transfers.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2004-2009 Jens Oknelid, paskharen@gmail.com
+ * Copyright © 2004-2010 Jens Oknelid, paskharen@gmail.com
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
