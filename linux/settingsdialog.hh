32240a08ee6788439ed7146f649cb592202644cb
troll.freedcpp <troll.freedcpp@98094be4-8a28-11de-a133-9d545b4d7674>
2010-05-29 08:50:34 +0000

    Ограничение скорости (core branch).

--- linux/settingsdialog.hh
+++ linux/settingsdialog.hh
@@ -65,6 +65,7 @@ class Settings:
 		void initAppearance_gui();
 		void initLog_gui();
 		void initAdvanced_gui();
+		void initBandwidthLimiting_gui();//NOTE: core 0.762
 		void addShare_gui(std::string path, std::string name, int64_t size);
 		void selectTextColor_gui(const int select);
 		void selectTextStyle_gui(const int select);
@@ -148,6 +149,7 @@ class Settings:
 		static void onDefaultThemeButton_gui(GtkWidget *widget, gpointer data);
 		static void onDefaultColorsSPButton_gui(GtkWidget *widget, gpointer data);
 		static void onDefaultFrameSPButton_gui(GtkWidget *widget, gpointer data);
+		static void onLimitSecondToggled_gui(GtkWidget *widget, gpointer data);//NOTE: core 0.762
 
 		// Client functions
 		void saveSettings_client();
