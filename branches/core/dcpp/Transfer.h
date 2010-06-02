beaef8cf7bd18a8bc008af579fae6b0814610239
troll.freedcpp <troll.freedcpp@98094be4-8a28-11de-a133-9d545b4d7674>
2010-06-02 11:20:38 +0000

    Ревизия 2153 dcplusplus (core branch).

--- branches/core/dcpp/Transfer.h
+++ branches/core/dcpp/Transfer.h
@@ -50,7 +50,7 @@ public:
 
 	int64_t getStartPos() const { return getSegment().getStart(); }
 
-	void resetPos() { pos = getStartPos(); }
+	void resetPos() { pos = 0; }
 
 	void addPos(int64_t aBytes, int64_t aActual) { pos += aBytes; actual+= aActual; }
 
