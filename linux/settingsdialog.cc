6f826bcc7a6193739bed1a69c6037d2f9a8022d7
troll.freedcpp <troll.freedcpp@98094be4-8a28-11de-a133-9d545b4d7674>
2011-08-27 08:17:36 +0000

    Ошибка сегментирования.

--- linux/settingsdialog.cc
+++ linux/settingsdialog.cc
@@ -3096,6 +3096,11 @@ bool Settings::validateUserCommandInput(const string &oldName)
 			return FALSE;
 		}
 	}
+	else if (FavoriteManager::getInstance()->findUserCommand(_("Separator"), "") != -1)
+	{
+		showErrorDialog(_("Command name already exists"));
+		return FALSE;
+	}
 
 	return TRUE;
 }
@@ -3920,6 +3925,7 @@ void Settings::moveUserCommand_client(string name, string hub, int pos)
 	{
 		FavoriteManager *fm = FavoriteManager::getInstance();
 		fm->moveUserCommand(fm->findUserCommand(name, hub), pos);
+		fm->save();
 	}
 }
 
