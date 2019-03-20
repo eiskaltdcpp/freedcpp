c9d2279a8eca83bacd149f7c7e3a3ffa238c0ef1
Boris Pek <tehnick-8@yandex.ru>
2019-03-21 02:29:28 +0300

    Stable release 0.1.0

--- linux/version.hh
+++ linux/version.hh
@@ -20,5 +20,5 @@
  */
 
 #define GUI_PACKAGE "freedcpp"
-#define GUI_VERSION_STRING "0.0.5"
-#define GUI_VERSION_BUILD_STRING "19"
+#define GUI_VERSION_STRING "0.1.0"
+#define GUI_VERSION_BUILD_STRING "0"
